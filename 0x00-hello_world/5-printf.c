a41760009fc0121f409bfa0ca95f5f48270c3813
jmbuvy <jemmymbuvi@gmail.com>
2023-07-13 07:51:55 -0700

    print commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints with proper grammar, but the outcome is a piece of art,
+ * Return: 0 (succss)
+ */
+int main(void)
+{
+puts("\"with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
