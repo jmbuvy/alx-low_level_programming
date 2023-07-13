fad0f427407f7c9ea350e008eaa08aabf2cce8e1
jmbuvy <jemmymbuvi@gmail.com>
2023-07-13 08:01:50 -0700

    printfcommit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - A program that prints with proper grammar, but the outcome is a piece of art,
+ * main - A program that prints this line
  * Return: 0 (succss)
  */
 int main(void)
 {
-printf("\"with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
