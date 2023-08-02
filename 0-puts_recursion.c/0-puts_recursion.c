b82933d3b3437a5cfc9226cd263ee819ab84fddc
jmbuvy <jemmymbuvi@gmail.com>
2023-08-02 09:17:03 -0700

    puts recursion commit

---
+++ 0-puts_recursion.c/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts_recursion -Function
+ * @s- The input
+ * Return: 0 always(success)
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+		_putchar('\n');
+}
