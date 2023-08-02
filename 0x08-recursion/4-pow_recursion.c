f8015d602601dbefd76f61d5f69066fd8db164cf
jmbuvy <jemmymbuvi@gmail.com>
2023-08-02 10:07:38 -0700

    power commit

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _pow_recursion -Function that Returns value of x raised to the power of y
+ * @x: Value to be raised
+ * @y: Power
+ * Return: Result of the POwer
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
