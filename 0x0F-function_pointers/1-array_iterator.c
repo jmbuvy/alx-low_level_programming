f80ae25e092ec835ee70684f36feaf5f7d0ee47e
jmbuvy <jemmymbuvi@gmail.com>
2023-08-16 03:30:17 -0700

    array pointer

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -6,7 +6,7 @@
  * @size: number of elements to print.
  * @action:pointer to print
  * Return:void(nothing)
- **/
+ */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
