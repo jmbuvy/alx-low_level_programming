86bbc4b68fc1d1d84ae37eb48b529d808d08ac38
jmbuvy <jemmymbuvi@gmail.com>
2023-08-16 03:15:08 -0700

    funpointers commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,9 +6,10 @@
  * @f:Pointer to the func
  * Return :(nothing)
  **/
-void print_name(char *name, void (*f)(char *));
+void print_name(char *name, void (*f)(char *))
 {
-	if(name == NULL || f == NULL)
-		RETURN;
+	if (name == NULL || f == NULL)
+		return;
+
 	f(name);
 }
