6c7dfa1c19bc827eb748553117ea19ad6f7b9eb3
jmbuvy <jemmymbuvi@gmail.com>
2023-08-17 03:49:08 -0700

    variadic functis commit

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - func returning sum of all its parameters.
+ * @n: number of parameters passed to the func.
+ * @...: A variable no. of parameters to calc sum of.
+ * Return: If n == 0 - 0.
+ *  Otherwise - the sum of all parameters.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i, sum = 0;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
+
+	va_end(ap);
+
+	return (sum);
+}
