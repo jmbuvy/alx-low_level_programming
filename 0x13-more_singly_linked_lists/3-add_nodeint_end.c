7c6756db959bf59db4734472d5a5728734837009
jmbuvy <jemmymbuvi@gmail.com>
2023-08-30 08:37:33 -0700

    add modeint commit

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+/**
+ * add_nodeint_end - Adding a node end of linked list
+ * @head: Points to the 1st element in list.
+ * @n: Data to be inserted.
+ * Return: The pointer tonew node, or NULL if otherwise
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new;
+	listint_t *temp = *head;
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+
+	while (temp->next)
+		temp = temp->next;
+
+	temp->next = new;
+
+	return (new);
+}
