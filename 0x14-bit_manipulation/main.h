23641005ae5174f87bc0d422f7edb6eb0bad3075
jmbuvy <jemmymbuvi@gmail.com>
2023-08-31 10:43:58 -0700

    binary unit commit

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,5 @@
-#ifndef main_h
-#define main_h
+#ifndef MAIN_H
+#define MAIN_H
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
